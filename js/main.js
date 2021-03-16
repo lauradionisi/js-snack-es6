b5b2d8181f3c571556655cd92a520cd3e14aae30
Laura <laura.dionisi91@gmail.com>
2021-03-16 19:44:05 +0100

    Snack ES6 V_02

--- js/main.js
+++ js/main.js
@@ -1,72 +1,94 @@
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-var biciclette = [
+var cycles = [
  {
-  nome: "Wilier",
-  peso: 10,
+  "nome": "Wilier",
+  "peso": 6,
  },
  {
-  nome: "Cannodale",
-  peso: 8,
+  "nome": "Cannodale",
+  "peso": 8,
  },
  {
-  nome: "Trek Emonda",
-  peso: 4,
+  "nome": "Trek Emonda",
+  "peso": 4,
  },
  {
-  nome: "Sava Phantom",
-  peso: 9,
+  "nome": "Sava Phantom",
+  "peso": 9,
  },
  
 ];
 
-for(var i= 0; i < biciclette.length; i++) {
- var [{nome, peso}] = biciclette;
- };
-console.log(`${peso}`);
+var ligther = cycles[0];
 
-// Creare un array di oggetti di squadre di calcio.
-// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+for (var i = 0; i < cycles.length; i++) {
+ if (cycles[i].peso < ligther.peso) {
+  ligther = cycles[i];
+ }
+}
+ console.log(`La bici più leggera pesa ${ligther.peso} kg`);
+
+// // Creare un array di oggetti di squadre di calcio.
+// // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// // Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+// // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
  
 var teams = [
- team1 = {
+ {
   "nome": "Roma",
   "punti": 0,
   "falli": 0,
  },
- team2 = {
+ {
   "nome": "Napoli",
   "punti": 0,
   "falli": 0,
  },
- team3 = {
+ {
   "nome": "Torino",
   "punti": 0,
   "falli": 0,
  },
- team4 = {
+ {
   "nome": "Crotone",
   "punti": 0,
   "falli": 0,
  },
 ];
 
+teams.forEach((element) => {
+ element.punti = Math.floor((Math.random() * 100) + 1);
+ element.falli = Math.floor((Math.random() * 10) + 1);
+});
 
-for (var key in teams) {
- var tmp = Math.floor(Math.random()* 100);
- var tmp2 = Math.floor(Math.random()* 10);
 
- team1.punti = tmp; 
- team2.punti = tmp;
- team3.punti = tmp;
- team4.punti = tmp;
- team1.falli = tmp2;
- team2.falli = tmp2;
- team3.falli = tmp2;
- team4.falli = tmp2;
- console.log(teams[key]);
+var secondArray = [];
+teams.forEach((element) => {
+ var {nome, falli} = element;
+ console.log(nome, falli);
+ secondArray.push({nome, falli});
+});
+
+console.log(secondArray);
+
+
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter
+
+
+var firstArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+function numbersPosition (arr, a, b) {
+  var arr2 = [];
+   arr.forEach((element, index) => {
+    if ((index >= a) && (index <= b)) {
+     arr2.push(element);
+ }
+ });
+    console.log(arr2);
 }
 
+numbersPosition(firstArray, 2, 7);
